9979c1bbf9a79828c1034c562e0141384ae97e7a
benhuang1998 <benjamin.haung@gmail.com>
2024-12-31 12:39:19 +0000

    lesson21

--- lesson21/main.py
+++ lesson21/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<strin:name>")
-def index(name:str="None"):
+def index(name:str=""):
     return f"<h1>Hello, {name}你好!</h1>"
 
 @app.route('/hello')
