53b0b27058796c21ce0bcc1ff69a7607ab0ffafe
benhuang1998 <benjamin.haung@gmail.com>
2024-12-31 11:44:30 +0000

    lesson21

--- lesson21/main.py
+++ lesson21/main.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hello, ben你好!</h1>"
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
